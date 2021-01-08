036de639bd1a76bb92f16b9ec1fba51d79e81983
fionayuqizhang <yu.qi.zhang@ocsbstudent.ca>
2021-01-08 14:45:40 +0000

    i

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ let radius = 3 * 3 * Math.PI
 let area = 3 * 3 * Math.PI
 let circumference = 2 * radius * Math.PI
 area = radius * radius * Math.PI
+document.getElementById('number').addEventListener('click', number)
 function number() {
-radius = document.getElementById("answer").value;
+radius = document.getElementById('answer').value;
 radius = parseInt(radius,10);
 area = (radius * radius * Math.PI)
 alert(area)
 }
 function cf() {
-let circle = document.getElementById("final").value;
-document.getElementById("final").innerHTML = circle;
+let circle = document.getElementById('final').value;
+document.getElementById('final').innerHTML = circle;
 radius = parseInt(radius,10);
 circumference = (2*radius*Math.PI)
 alert(circumference)
