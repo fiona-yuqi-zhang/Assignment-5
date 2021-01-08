53a3ebd2966eeee6eb05f49f2f0b7c2d7664ee55
fionayuqizhang <yu.qi.zhang@ocsbstudent.ca>
2021-01-08 14:31:55 +0000

    i

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-
-  let radius=3*3*Math.PI
-let area=3*3*Math.PI
-let circumference=2*radius*Math.PI
+let radius = 3 * 3 * Math.PI
+let area = 3 * 3 * Math.PI
+let circumference = 2 * radius * Math.PI
 area = radius * radius * Math.PI
-function CalculateArea(){
-    var radius =document.form1.txtRadius.value;
-    document.write("<P>The area of the circle is " + (radius * radius * Math.PI) + "</p>");
-    document.write("<P>The circumference of the circle is " + (2 * radius * Math.PI) + "</p>");
-    }
 function number() {
-  radius=document.getElementById("answer").value;
-    radius=parseInt(radius,10);
-  area=(radius * radius * Math.PI)
-  alert(area)
+radius = document.getElementById("answer").value;
+radius = parseInt(radius,10);
+area = (radius * radius * Math.PI)
+alert(area)
 }
 function cf() {
-  let circle = document.getElementById("final").value;
-  document.getElementById("final").innerHTML = circle;
-  radius=parseInt(radius,10);
-  circumference=(2*radius*Math.PI)
-  alert(circumference)
+let circle = document.getElementById("final").value;
+document.getElementById("final").innerHTML = circle;
+radius = parseInt(radius,10);
+circumference = (2*radius*Math.PI)
+alert(circumference)
 }
